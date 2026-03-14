68877e35a09144ed2f81e1f6b2f32cd7c1b423ba
DmitriX <dodge.241294@gmail.com>
2026-03-14 23:02:23 +0300

    Add nixie

---
+++ src/app/assets/nixie/nixie.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nixie',
+  imports: [],
+  templateUrl: './nixie.html',
+  styleUrl: './nixie.scss',
+})
+export class Nixie {
+
+}
