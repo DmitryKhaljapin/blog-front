8dba922fe2c5bc591d8c0fc3c7a706806e135dbf
DmitriX <dodge.241294@gmail.com>
2026-03-15 22:14:05 +0300

    Add tumbler

---
+++ src/app/common-ui/tumbler/tumbler.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-tumbler',
+  imports: [RouterModule],
+  templateUrl: './tumbler.html',
+  styleUrl: './tumbler.scss',
+})
+export class Tumbler {
+  @Input() to: string;
+  isActive: boolean = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        const currentUrl = event.urlAfterRedirects;
+        if (currentUrl.includes(this.to)) this.isActive = true;
+      });
+  }
+}
