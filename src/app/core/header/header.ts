3b7304f2fd120fdeed4126d1922625cdb0b53ea5
DmitriX <dodge.241294@gmail.com>
2026-03-15 22:15:43 +0300

    Add header

--- src/app/core/header/header.ts
+++ src/app/core/header/header.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { GearBox } from '../../assets/gear-box/gear-box';
 import { Gear } from '../../assets/gear/gear';
 import { Steam } from '../../assets/steam/steam';
-import { NavLink } from '../../common-ui/nav-link/nav-link';
 import { Pipe } from '../../assets/pipe/pipe';
+import { Tumbler } from '../../common-ui/tumbler/tumbler';
+import { NavLink } from '../../common-ui/nav-link/nav-link';
+import { Nixie } from '../../assets/nixie/nixie';
 
 @Component({
   selector: 'app-header',
-  imports: [NavLink, GearBox, Gear, Steam, Pipe],
+  imports: [NavLink, GearBox, Gear, Steam, Pipe, Tumbler, Nixie],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
